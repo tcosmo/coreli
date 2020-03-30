a1a17547df7d12c9555f5ca3049dab8b5517a2b8
tcosmo <tristan.sterin@ens-lyon.fr>
2020-03-30 12:40:41 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coreli-tcosmo",
+    version="0.0.1",
+    author="Tristan Stérin",
+    author_email="tristan.sterin@mu.ie",
+    description="The Collatz Research Library provides tools for experimenting and testing hypothesises related to the Collatz Process.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tcosmo/coreli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
