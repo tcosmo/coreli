492a8bf6466b38de16bbd369567f11d5c96a8b70
Tristan Stérin <tristan.sterin@mu.ie>
2022-10-30 18:21:50 +0100

    coreli

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="coreli-tcosmo",
+    name="coreli",
     version="0.0.2",
     author="Tristan Stérin",
     author_email="tristan.sterin@mu.ie",
