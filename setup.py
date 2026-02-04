ee85b0018f94b750dc56f7d511fc749cfb143639
Tristan Stérin <tristan@prgm.dev>
2026-02-04 09:50:31 +0100

    0.0.7

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    install_requires = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
 setuptools.setup(
     name="coreli",
-    version="0.0.6",
+    version="0.0.7",
     author="Tristan Stérin",
     author_email="tristan.sterin@mu.ie",
     description="The Collatz Research Library provides tools for experimenting and testing hypothesises related to the Collatz Process.",
@@ -18,6 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
-    install_requires=["sympy==1.11.1", "drawsvg==1.8.3", "ipywidgets==8.0.2"],
+    python_requires=">=3.9",
+    install_requires=install_requires,
 )
